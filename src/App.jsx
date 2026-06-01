e548865069651df4baf33b123acdcb13995451b6
JorgeVallejos14 <jorge.vallejos14@inacapmail.cl>
2026-05-31 21:48:28 -0400

    agregar listado de jugadores con map

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import jugadores from "./data/jugadores"
 import PlayerCard from "./components/PlayerCard"
 function App() {
   return (
     <div>
       <h1>Mobius FC</h1>
-
-      <PlayerCard
-       nombre="Sonic"
-       posicion="Delantero Centro"
-       numero="9"
-      />
+      {
+  jugadores.map((jugador) => (
+    <PlayerCard
+      nombre={jugador.nombre}
+      posicion={jugador.posicion}
+      numero={jugador.numero}
+    />
+  ))
+}
     </div>
   )
 }
---
+++ src/data/jugadores.js
@@ -0,0 +1,24 @@
+const jugadores = [
+  {
+    nombre: "Sonic",
+    posicion: "Delantero Centro",
+    numero: 9
+  },
+  {
+    nombre: "Tails",
+    posicion: "Arquero",
+    numero: 1
+  },
+  {
+    nombre: "Knuckles",
+    posicion: "Defensa Central",
+    numero: 4
+  },
+  {
+    nombre: "Sally",
+    posicion: "Presidenta",
+    numero: "-"
+  }
+]
+
+export default jugadores
