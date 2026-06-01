1ea4c8c02659d8efef4640d723df2f4abbd07a35
JorgeVallejos14 <jorge.vallejos14@inacapmail.cl>
2026-05-31 22:21:57 -0400

    Agregar jugadores favoritos y encontrados

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import PlayerCard from "./components/PlayerCard"
 import {useState} from "react"
 function App() {
   const [busqueda, setBusqueda] = useState("")
+  const [favoritos, setFavoritos] = useState([])
+  const agregarFavorito = (nombre) => {
+  if (!favoritos.includes(nombre)) {
+    setFavoritos([...favoritos, nombre])
+  }
+}
   const jugadoresFiltrados = jugadores.filter((jugador) =>
   jugador.nombre.toLowerCase().includes(busqueda.toLowerCase())
 )
@@ -15,12 +21,15 @@ function App() {
       value={busqueda}
       onChange={(e) => setBusqueda(e.target.value)}
     />
+    <p>Jugadores encontrados: {jugadoresFiltrados.length}</p>
+    <p>Favoritos: {favoritos.length}</p>
       {
   jugadoresFiltrados.map((jugador) => (
     <PlayerCard
       nombre={jugador.nombre}
       posicion={jugador.posicion}
       numero={jugador.numero}
+      agregarFavorito={agregarFavorito}
     />
   ))
 }
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -4,6 +4,9 @@ function PlayerCard(props) {
             <h2>{props.nombre}</h2>
             <p>Posición: {props.posicion}</p>
             <p>Número: {props.numero}</p>
+            <button
+            onClick={() => props.agregarFavorito(props.nombre)}>⭐ Favorito
+            </button>
         </div>
     )
 }
