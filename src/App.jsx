c23093b97fb4698ce8c241e5027271d6648c4d1b
JorgeVallejos14 <jorge.vallejos14@inacapmail.cl>
2026-05-31 21:56:14 -0400

    agregar useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import jugadores from "./data/jugadores"
 import PlayerCard from "./components/PlayerCard"
+import {useState} from "react"
 function App() {
+  const [busqueda, setBusqueda] = useState("")
+  const jugadoresFiltrados = jugadores.filter((jugador) =>
+  jugador.nombre.toLowerCase().includes(busqueda.toLowerCase())
+)
   return (
     <div>
       <h1>Mobius FC</h1>
+      <input
+      type="text"
+      placeholder="Buscar jugador..."
+      value={busqueda}
+      onChange={(e) => setBusqueda(e.target.value)}
+    />
       {
-  jugadores.map((jugador) => (
+  jugadoresFiltrados.map((jugador) => (
     <PlayerCard
       nombre={jugador.nombre}
       posicion={jugador.posicion}
